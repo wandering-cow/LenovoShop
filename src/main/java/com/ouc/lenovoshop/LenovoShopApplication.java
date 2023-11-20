1bf8347180a73eda72c9928fab7ff380209d77e8
wanderingcow <994392974@qq.com>
2023-11-20 17:08:08 +0800

    第一次提交

---
+++ src/main/java/com/ouc/lenovoshop/LenovoShopApplication.java
@@ -0,0 +1,13 @@
+package com.ouc.lenovoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenovoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LenovoShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ouc/lenovoshop/LenovoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouc.lenovoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenovoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
