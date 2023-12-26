9e850317f27d6e9709758940cfa6cf8ca7247765
wanderingcow <994392974@qq.com>
2023-12-26 21:05:50 +0800

    细化角色数据反馈

--- springboot/src/main/java/com/ouc/lenovoshop/common/enums/RoleEnum.java
+++ springboot/src/main/java/com/ouc/lenovoshop/common/enums/RoleEnum.java
@@ -3,6 +3,8 @@ package com.ouc.lenovoshop.common.enums;
 public enum RoleEnum {
     // 管理员
     ADMIN,
+    // 商家
     BUSINESS,
+    // 用户
     USER,
 }
--- springboot/src/main/java/com/ouc/lenovoshop/common/enums/StatusEnum.java
+++ springboot/src/main/java/com/ouc/lenovoshop/common/enums/StatusEnum.java
@@ -2,10 +2,12 @@ package com.ouc.lenovoshop.common.enums;
 
 public enum StatusEnum {
     CHECKING("审核中"),
-    CHECK_NO("审核不通过"),
     CHECK_OK("审核通过"),
+    CHECK_NO("审核不通过"),
     ;
+
     public String status;
+
     StatusEnum(String status) {
         this.status = status;
     }
