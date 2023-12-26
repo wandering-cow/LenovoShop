85a17391273e704e59e486222704ac27f0a76b17
wanderingcow <994392974@qq.com>
2023-12-26 21:00:38 +0800

    web拦截更新

--- springboot/src/main/java/com/ouc/lenovoshop/common/config/WebConfig.java
+++ springboot/src/main/java/com/ouc/lenovoshop/common/config/WebConfig.java
@@ -19,6 +19,12 @@ public class WebConfig implements  WebMvcConfigurer {
                 .excludePathPatterns("/")
                 .excludePathPatterns("/login")
                 .excludePathPatterns("/register")
-                .excludePathPatterns("/files/**");
+                .excludePathPatterns("/files/**")
+                .excludePathPatterns("/type/**")
+                .excludePathPatterns("/notice/selectAll")
+                .excludePathPatterns("/goods/**")
+                .excludePathPatterns("/goods/**")
+                .excludePathPatterns("/comment/selectByGoodsId/**")
+        ;
     }
 }
